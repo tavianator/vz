7a66711e95a97b3389a29b09aaead8cb18126e80
Tavian Barnes <tavianator@gmail.com>
2010-10-11 18:21:25 -0400

    Add/fix copyright headers.

--- tests/EquationSystem.cpp
+++ tests/EquationSystem.cpp
@@ -1,3 +1,22 @@
+/*************************************************************************
+ * Copyright (C) 2010 Tavian Barnes <tavianator@gmail.com>               *
+ *                                                                       *
+ * This file is part of The vZ Test Suite.                               *
+ *                                                                       *
+ * The vZ Test Suite is free software; you can redistribute it and/or    *
+ * modify it under the terms of the GNU General Public License as        *
+ * published by the Free Software Foundation; either version 3 of the    *
+ * License, or (at your option) any later version.                       *
+ *                                                                       *
+ * The vZ Test Suite is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ * General Public License for more details.                              *
+ *                                                                       *
+ * You should have received a copy of the GNU General Public License     *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *************************************************************************/
+
 #include "vZ.hpp"
 #include <cmath>
 #include <cstdlib>
@@ -23,7 +42,10 @@ main()
   y[0] = 1.0;
   y[1] = 1.0;
   vZ::GenericDP45Integrator<Y> integrator(f);
-  integrator.tol(1e-6).y(y).x(0.0).h(0.06);
+  integrator.tol(1e-6)
+            .y(y)
+            .x(0.0)
+            .h(0.06);
 
   integrator.integrate(2.0);
 
